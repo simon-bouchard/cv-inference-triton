1d68f6ea83e0d8045efacf5ac637690fe9593f98
Simon Bouchard <simon.bouchard31@gmail.com>
2026-05-03 01:26:50 -0400

    feat: add custom Dockerfile with pillow, update run script with shm-size

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM nvcr.io/nvidia/tritonserver:23.08-py3
+RUN pip install pillow
--- scripts/run_triton.sh
+++ scripts/run_triton.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-docker run --rm \
+docker run --rm -d \
   --gpus all \
+  --shm-size=512m \
   -p 8000:8000 \
   -p 8001:8001 \
   -p 8002:8002 \
   -v $(pwd)/model_repository:/models \
-  nvcr.io/nvidia/tritonserver:23.08-py3 \
+  --name triton \
+  tritonserver-cv:23.08 \
   tritonserver --model-repository=/models
